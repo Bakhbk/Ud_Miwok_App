fc02c85a9f6a9fbf357c9440045e61aad1f6b799
Bakhbk <bakhbk@gmail.com>
2018-05-20 23:10:17 +0300

    update after finished arrays lessons

---
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -0,0 +1,30 @@
+package com.example.android.miwok;
+/**
+ * {@link Word} represents a vocabulary word that the user wants to learn.
+ * It contains a default translation and a Miwok translation fo that word.
+ * */
+
+public class Word {
+
+    // Default translation for the word
+    private String mDefaultTranslation;
+
+    // Miwok translation for the word
+    private String mMiwokTranslation;
+
+    public Word(String defaultTranslation, String miwokTranslation) {
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+
+    // Get the default translation of the word.
+    public String getDefaultTranslation() {
+        return mDefaultTranslation;
+    }
+
+    // Get the Miwok translation of the word.
+    public String getMiwokTranslation() {
+        return mMiwokTranslation;
+    }
+
+}
